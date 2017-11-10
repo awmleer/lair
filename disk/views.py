9ac624315242c25cbd6210e48dc8ea8b5267ad19
awmleer <awmleer@sparker.xyz>
2017-11-10 11:20:56 +0800

    qiniu sdk test

--- disk/views.py
+++ disk/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.conf import settings
+from qiniu import Auth
+from qiniu import BucketManager
 
-# Create your views here.
+q = Auth(settings.QINIU['accessKey'], settings.QINIU['secretKey'])
+bucket = BucketManager(q)
+bucket_name = 'zjulibrary'
+
+
+def test(request):
+    ret, eof, info = bucket.list(bucket=bucket_name, prefix='assets/', marker=None, delimiter='/')
+    print(ret)
+    # print(info)
+    dirs = ret['commonPrefixes'] if 'commonPrefixes' in ret else []
+    files = ret['items'] if 'items' in ret else []
+    print(dirs)
+    print(files)
+    return JsonResponse(ret)

e5d6aea36b0a1b533365a9aa8f4ad92e993f809c
awmleer <awmleer@sparker.xyz>
2017-11-11 21:06:12 +0800

    fileSize filter

--- disk/templatetags/diskFilters.py
+++ disk/templatetags/diskFilters.py
@@ -6,3 +6,14 @@ register = template.Library()
 def cutPrefix(value, arg):
     return value.replace(arg, '', 1)
 
+@register.filter(name='fileSize')
+def fileSize(value):
+    value=int(value)
+    if value>=1000000000:
+        return "%.1f G" % (value/1000000000)
+    if value>=1000000:
+        return "%.1f M" % (value/1000000)
+    if value>=1000:
+        return "%.1f K" % (value/1000)
+    else:
+        return "%d B" % value

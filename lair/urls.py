059bcd069495f388f2c8af0998e16e711f1f0151
awmleer <awmleer@sparker.xyz>
2017-12-15 15:05:10 +0800

    login page html

--- lair/urls.py
+++ lair/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^disk/', include('disk.urls'))
+    url(r'^disk/', include('disk.urls')),
+    url(r'^account/', include('account.urls')),
 ]
